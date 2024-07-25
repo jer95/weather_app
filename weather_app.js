7c3d9a5c8a97ece44c682197dac6f2e6f26d2a55
Jeremiah Dulla <jeremiahdulla@gmail.com>
2024-07-24 23:12:12 -0400

    refactor: hardcode html & remove unused functions

--- weather_app.js
+++ weather_app.js
@@ -4,22 +4,22 @@ async function getWeatherData(location) {
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=X93G6HR8DTW52466TMUQ94YQC&contentType=json`
   );
   const response = await responseJSON.json();
+  document.getElementById("loading").style.display = "none";
   return response;
 }
 
-async function sortData(location) {
-  // get weather data we need
-  const data = await getWeatherData(location);
-  const parsedData = data.days[0];
-  document.getElementById("loading").style.display = "none";
-  return parsedData;
-}
-
 function searchWeather() {
-  // dom elements
+  // get dom elements
   const searchBtn = document.getElementById("search-btn");
   const search = document.getElementById("search");
   const cityName = document.getElementById("city");
+  const conditions = document.getElementById("conditions");
+  const temp = document.getElementById("temperature");
+  const feelsLike = document.getElementById("feels-like");
+  const wind = document.getElementById("wind");
+  const humidity = document.getElementById("humidity");
+  const date = document.getElementById("date-time");
+
   // searchBtn listener
   searchBtn.addEventListener("click", async () => {
     document.getElementById("loading").style.display = "";
@@ -28,67 +28,27 @@ function searchWeather() {
       document.getElementById("container").remove();
     }
     try {
-      const data = await sortData(search.value);
+      const data = await getWeatherData(search.value);
       console.log(data);
-      createElements();
-      // get created elements
-      const temp = document.getElementById("temp");
-      const feelsLike = document.getElementById("feelslike");
-      const icon = document.getElementById("icon");
-      const wind = document.getElementById("wind");
-      const pop = document.getElementById("pop");
-      const humidity = document.getElementById("humidity");
       // assign values
-      cityName.textContent = search.value;
-      temp.innerHTML = `${data.temp}&deg;F`;
-      feelsLike.textContent = `Feels Like ${data.feelslikemax}`;
-      icon.textContent = data.conditions;
-      wind.textContent = `Wind: ${data.windspeed} km/h`;
-      pop.textContent = `Precipitation % : ${data.precipprob}%`;
-      humidity.textContent = `Humidity: ${data.humidity}`;
+      cityName.textContent = data.resolvedAddress;
+      conditions.textContent = data.currentConditions.conditions;
+      date.textContent = new Date().toString().split(" ").slice(0, 5).join(" ");
+      temp.innerHTML = `${data.currentConditions.temp}&deg;F`;
+      feelsLike.innerHTML = `Feels Like: ${data.currentConditions.feelslike}&deg;F`;
+      wind.textContent = `Wind: ${data.currentConditions.windspeed} km/h`;
+      humidity.textContent = `Humidity: ${data.currentConditions.humidity}%`;
+      search.value = "";
     } catch {
       cityName.textContent = "Please enter a valid city name.";
     }
   });
 }
 
-function createElements() {
-  const section = document.getElementById("section");
-
-  // create html elements
-  const mainContainer = document.createElement("div");
-  const container1 = document.createElement("div");
-  const container2 = document.createElement("div");
-  const temp = document.createElement("p");
-  const feelsTemp = document.createElement("p");
-  const icon = document.createElement("p");
-  const wind = document.createElement("p");
-  const pop = document.createElement("p");
-  const humidity = document.createElement("p");
-
-  //assign attributes
-  mainContainer.setAttribute("id", "container");
-  mainContainer.classList.add("main-container");
-  container1.classList.add("container1");
-  container2.classList.add("container2");
-  temp.setAttribute("id", "temp");
-  temp.classList.add("temp");
-  feelsTemp.setAttribute("id", "feelslike");
-  wind.setAttribute("id", "wind");
-  icon.setAttribute("id", "icon");
-  pop.setAttribute("id", "pop");
-  humidity.setAttribute("id", "humidity");
-
-  //append elements
-  container1.appendChild(temp);
-  container1.appendChild(icon);
-  container1.appendChild(feelsTemp);
-  container2.appendChild(wind);
-  container2.appendChild(pop);
-  container2.appendChild(humidity);
-  mainContainer.appendChild(container1);
-  mainContainer.appendChild(container2);
-  section.appendChild(mainContainer);
-}
-
-searchWeather();
+document.addEventListener("DOMContentLoaded", () => {
+  searchWeather();
+  // display default location
+  document.getElementById("search").value = "Toronto";
+  document.getElementById("search-btn").click();
+  document.getElementById("search").value = "";
+});
